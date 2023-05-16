d0b2cb6e2da1fd3f816036cd37c6f711bee85c84
Mayank <7mayank.agarwal@gmail.com>
2023-05-16 23:35:29 +0530

    boilerplate

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
+// const user=new User()
+// console.log(user)
+// const company=new Company()
+// console.log(company)
+
+new google.maps.Map(document.getElementById('map') as HTMLElement,
+{
+  zoom: 1,
+  center:
+  {
+    lat: 0,
+    lng: 0
+  }
+})
